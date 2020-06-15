44889f3f939727316b25192be5dc363ccfd02e8d
ynpcamp4 <66397362+ynpcamp4@users.noreply.github.com>
2020-06-15 11:49:19 -0700

    some final revisions and cleaning things up

--- shopping_cart.py
+++ shopping_cart.py
@@ -23,9 +23,14 @@ products = [
     {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
-#
-# INFO CAPTURE / INPUT
-#
+print("----------------------------------------")
+print("Yosemite Village Store")
+print("----------------------------------------")
+print("Website: shop.travelyosemite.com/")
+print("Phone: (209) 372-1253")
+
+def to_usd(my_price):
+    return f"${my_price:,.2f}" #> $12,000.71
 
 total_price = 0
 selected_ids = []
@@ -34,38 +39,36 @@ while True:
     selected_id = input("Please input a product identifier: ")
     if selected_id == "DONE":
         break
-    else:
-        # matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-        # matching_product = matching_products[0]
-        # total_price = total_price + matching_product["price"]
-        # # print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))       
+    else: 
         selected_ids.append(selected_id)
 
-#
-# INFO DISPLAY / OUTPUT
-#
-
-# print(selected_ids)
-
 for selected_id in selected_ids:
-      matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-      matching_product = matching_products[0]
-      total_price = total_price + matching_product["price"]
-      print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))     
+    matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+    matching_product = matching_products[0]
+    total_price = total_price + matching_product["price"]
+    print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+
+    taxrate = 0.775
+    tax = total_price * taxrate
+    sum_total = tax + total_price
+
+def lookup_product_by_id(selected_id):
+    matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+    return matching_products[0]
 
 print("TOTAL PRICE: " + str(total_price))
 
-# A grocery store name of your choice
-# A grocery store phone number and/or website URL and/or address of choice
-# The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2019-06-06 11:31 AM)
-# The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $1.50)
-# The total cost of all shopping cart items, formatted as US dollars and cents (e.g. $4.50), calculated as the sum of their prices
-# The amount of tax owed (e.g. $0.39), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
-# The total amount owed, formatted as US dollars and cents (e.g. $4.89), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
-# A friendly message thanking the customer and/or encouraging the customer to shop again
+price = 0
 
-
-
-
-# def to_usd(my_price):
-#     return f"${my_price:,.2f}" #> $12,000.71
+from datetime import datetime
+now = datetime.now()
+dt_string = now.strftime("%m/%d/%Y %H:%M:%S")
+print("CHECKOUT " + dt_string)	
+print("----------------------------------------")
+print("What's in Your Basket: ")
+print("----------------------------------------")
+print ("Amount of purchase:" + '${0:.2f}'.format(total_price))
+print ("YNP Sales Tax (6.25 %): " f"${tax:,.2f}")
+print ("The total price is:", f"${sum_total:,.2f}")
+print("----------------------------------------")
+print("Thank you for shopping at Yosemite Village Store. Enjoy your time in the park. Stay safe!")
